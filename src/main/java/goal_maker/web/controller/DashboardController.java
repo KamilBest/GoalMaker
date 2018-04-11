7abd49ef327fd74d807e9c24555b5a5048549dc0
KamilBest <kamil.besst@gmail.com>
2018-04-11 22:35:53 +0200

    Naprawiono logowanie

--- src/main/java/goal_maker/web/controller/DashboardController.java
+++ src/main/java/goal_maker/web/controller/DashboardController.java
@@ -40,7 +40,7 @@ public class DashboardController {
     UserFinancesService userFinancesService;
     private List<Goal> realisedGoals = new ArrayList<>();
 
-    @RequestMapping(value = {"/dashboard","/index"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/","/dashboard","/index"}, method = RequestMethod.GET)
     public String showFrontPage(Model model) {
         model.addAttribute("location", "dashboard");
 
